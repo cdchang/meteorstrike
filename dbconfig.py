bb306ef0abcda8bcb99cb144b023de1292148a5b
Caroline Chang <cdc222@cornell.edu>
2017-08-17 22:57:29 -0400

    configuring *mysql* sql dialect in the setup file in global and project

--- dbconfig.py
+++ dbconfig.py
@@ -1,16 +1,18 @@
 import pymysql
-import dbconfig
-connection = pymysql.connect(host='localhost', user="root", passwd=123)
+#import dbconfig <- only need this when not hardcoding user & password
+#write fake password before uploading to Github
+connection = pymysql.connect(host='localhost', user="root", passwd="123")
 
 try:
     with connection.cursor() as cursor: ##the beginning of sql code
 
-        sql = "CREATE DATABASE IF NOT EXISTS meteorstrike"
+        #needed to set the SQL dialect to MySQL for both global and project in order for sql to get recognized
+        sql = "CREATE DATABASE meteorstrike IF NOT EXISTS meteorstrike_db"
         cursor.execute(sql)
-        sql = """CREATE TABLE IF NOT EXISTS meteorstrike.strikes (
+        sql = """CREATE TABLE IF NOT EXISTS meteorstrike (
 id int NOT NULL AUTO_INCREMENT,
-latitude FLOAT(10,6)
-longitude FLOAT(10,6)
+latitude FLOAT(10,6),
+longitude FLOAT(10,6),
 date DATETIME,
 category VARCHAR(50),
 description VARCHAR(1000),
